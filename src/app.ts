ee727226010727b79fa77a7abbfeb44b2bea945b
backmeupplz <backmeupplz@gmail.com>
2018-08-06 13:29:29 -0700

    trying to fix whitespace

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ bot.startPolling()
 
 // Setup the reply middleware
 bot.use((ctx, next) => {
-  ctx.replyWithMarkdown(`Update\n\`\`\`\uFEFF${JSON.stringify((<any>ctx).update, undefined, 2)}\`\`\``)
+  ctx.replyWithMarkdown(`\`\`\`\uFEFF\uFEFF\uFEFF${JSON.stringify((<any>ctx).update, undefined, 2)}\`\`\``)
   // Continue bot execution
   next()
 })
