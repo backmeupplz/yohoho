0754c91566196931b27c52e118095b61651d5915
Nikita Kolmogorov <backmeupplz@gmail.com>
2025-04-11 08:26:59 -0700

    Replace Telegraf with Grammy for Telegram bot implementation

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,14 @@
-// Get environment variables
 import * as dotenv from 'dotenv'
 dotenv.config({ path: `${__dirname}/../.env` })
 
-// Dependencies
-import { Telegraf, ContextMessageUpdate } from 'telegraf'
-const telegraf = require('telegraf')
+import { Bot } from 'grammy'
 
-// Setup the bot
-const bot: Telegraf<ContextMessageUpdate> = new telegraf(process.env.TOKEN, {
-  username: process.env.USERNAME,
-  channelMode: true,
-})
-bot.startPolling()
+const bot = new Bot(process.env.TOKEN)
+bot.start()
 
-// Setup the reply middleware
 bot.use((ctx, next) => {
-  ctx.replyWithMarkdown(`\`\`\` ${JSON.stringify((<any>ctx).update, undefined, 2)}\`\`\``)
-  // Continue bot execution
+  ctx.reply(`\`\`\` ${JSON.stringify(ctx.update, undefined, 2)}\`\`\``, {
+    parse_mode: 'MarkdownV2',
+  })
   next()
 })
